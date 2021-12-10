774b5a6bb8cf4287d872f911c82eaac95eb76ae2
Ikhwan <ikhwanrts@gmail.com>
2021-12-10 17:20:33 +0700

    first commit

---
+++ src/components/todo/Todo.js
@@ -0,0 +1,38 @@
+import './Todo.css';
+import { useState } from 'react';
+import TodoList from '../todo-list/TodoList';
+import TodoCreate from '../todo-create/TodoCreate';
+
+
+const Todo = () => {
+  // const todos = [
+  //   { id: 1, title: 'Eat'},
+  //   { id: 2, title: 'Sleep'},
+  //   { id: 3, title: 'Code'},
+  // ];
+
+  const [getTodos, setTodos] = useState([
+    { id: 1, title: 'Eat'},
+    { id: 2, title: 'Sleep'},
+    { id: 3, title: 'Code'},
+  ]);
+
+
+  const eventCreateTodo = (todo) => {
+    setTodos(getTodos.concat(todo));
+
+    console.log(getTodos);
+    // console.log(todos);
+  }
+
+  return (
+    <div>
+      <h3>Todo List</h3>
+      <TodoCreate onCreateTodo={eventCreateTodo} />
+      <TodoList dataTodos={getTodos} />
+    </div>
+  )
+}
+
+
+export default Todo
